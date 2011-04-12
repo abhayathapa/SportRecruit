10693605d5c21011de5681e202c744dc0d0c515b
Your Name <abhaya.thapa@sprout-technology.com>
2011-04-12 10:25:07 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportRecruit::Application.config.secret_token = '41a6cffba79483baf072cfe826240383725bf1297caefb2d18448770e27a9034e769a30e8ef87826c72de5e5ff1a762c47b1522f3dd180891636b02a1988a5e7'
